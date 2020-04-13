50f116c581743e3e63d26cfc9f4a76e83292cd8f
Fernanda Leite <fernandacarolineleite360@gmail.com>
2020-04-13 19:45:39 -0300

    Update project
    Project organization changes and backend changes.

--- server.js
+++ server.js
@@ -1,15 +1,64 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const server = express();
 
+const ideas = [
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    title: "Cursos de Programação",
+    category: "Estudo",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, veritatis in qui ipsa porro hic corrupti!",
+    url: "https://rocketseat.com.br"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+    title: "Exercícios",
+    category: "Saúde",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, veritatis in qui ipsa porro hic corrupti!",
+    url: "https://rocketseat.com.br"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+    title: "Meditação",
+    category: "Mentalidade",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, veritatis in qui ipsa porro hic corrupti!",
+    url: "https://rocketseat.com.br"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+    title: "Karaoke",
+    category: "Diversão em Fámilia",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, veritatis in qui ipsa porro hic corrupti!",
+    url: "https://rocketseat.com.br"
+  }
+];
+
 server.use(express.static("public"));
 
+nunjucks.configure("views", {
+  express: server,
+  noCache: true, // não usar cache apenas em tempo de desenvolvimento
+});
+
 server.get("/", function (req, res) {
-  return res.sendFile(__dirname + "/index.html");
+  const reversedIdeas = [...ideas].reverse();
+
+  let lastIdeas = [];
+
+  for (let idea of reversedIdeas) {
+    if (lastIdeas.length < 2) {
+      lastIdeas.push(idea);
+    }
+  }
+
+  return res.render("index.html", { ideas: lastIdeas });
 });
 
 server.get("/ideas", function (req, res) {
-  return res.sendFile(__dirname + "/ideas.html");
+  const reversedIdeas = [...ideas].reverse();
+
+  return res.render("ideas.html", { ideas: reversedIdeas });
 });
 
 server.listen(3000);
